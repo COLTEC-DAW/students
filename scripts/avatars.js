eb6167ebf5d324201a3131822c15a84c2cc09e29
Aidan Feldman <aidan.feldman@gsa.gov>
2015-04-16 01:02:35 -0400

    user styling

--- scripts/avatars.js
+++ scripts/avatars.js
@@ -2,13 +2,18 @@ $(function() {
   $('.js-student').each(function() {
     var $student = $(this);
     var username = $student.data('username');
+    var $avatar = $student.find('.js-avatar');
 
     $.ajax({
       url: 'https://api.github.com/users/' + username,
       dataType: 'json',
+      timeout: 2000,
       cache: true,
       success: function(data) {
-        $student.prepend('<img class="avatar" src="' + data.avatar_url + '"/>');
+        $avatar.attr('src', data.avatar_url);
+      },
+      error: function() {
+        $avatar.attr('src', 'https://github.com/identicons/' + username + '.png');
       }
     })
   });
