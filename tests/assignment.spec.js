b3d63b168fb9138dd2762ece7e425459aae585c4
João Eduardo Montandon <edu.montandon@gmail.com>
2022-02-11 19:46:32 +0000

    Updated jekyll environment variable

--- tests/assignment.spec.js
+++ tests/assignment.spec.js
@@ -10,17 +10,17 @@ beforeAll(() => {
     $ = require('jquery')(dom.window);
 });
 
-test('Adicionado arquivo json', () => {
+test('1. Adicionado arquivo json', () => {
     let studentElement = $('.js-student')[0];
     expect(studentElement).toBeDefined();       
 });
 
-test('Adicionado texto de descrição', () => {
+test('2. Adicionado texto de descrição', () => {
     let introText = $('.js-student .info p')[0].textContent.trim();
     expect(introText.length).toBeGreaterThan(0);
 });
 
-test('Adicionado emoji', () => {
+test('3. Adicionado emoji', () => {
     let emojiElement = $('.js-student .em')[0];
     expect(emojiElement).toBeDefined();
 });
