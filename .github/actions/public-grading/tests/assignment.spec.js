55cdb26b9043110ba46d056cf0a97bdcb09aef6f
João Eduardo Montandon <edu.montandon@gmail.com>
2022-02-11 00:59:54 +0000

    Added Dockerfile

---
+++ .github/actions/public-grading/tests/assignment.spec.js
@@ -0,0 +1,26 @@
+const jsdom = require('jsdom');
+const fs = require('fs');
+
+
+let htmlFile = undefined;
+let $ = undefined;
+beforeAll(() => {
+    let html = fs.readFileSync('../../../_site/index.html');
+    let dom = new jsdom.JSDOM(html);
+    $ = require('jquery')(dom.window);
+});
+
+test('Adicionado arquivo json', () => {
+    let studentElement = $('.js-student')[0];
+    expect(studentElement).toBeDefined();       
+});
+
+test('Adicionado texto de descrição', () => {
+    let introText = $('.js-student .info p')[0].textContent.trim();
+    expect(introText.length).toBeGreaterThan(0);
+});
+
+test('Adicionado emoji', () => {
+    let emojiElement = $('.js-student .em')[0];
+    expect(emojiElement).toBeDefined();
+});
